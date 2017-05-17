ee966d9613ed089a55da8b5fb11fced47c3abfb0
Holly Schinsky <hollyschinsky@gmail.com>
2017-05-17 19:42:09 +0200

    Icon updates

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,5 +1,8 @@
 // use a cacheName for cache versioning
-var cacheName = 'v1:todos-pwa-app';
+var cacheName = 'todos_offline_cache:v5'
+var goodCaches = [];
+goodCaches.push(cacheName);
+goodCaches.push('CordovaAssets');
 
 // during the install phase you usually want to cache static assets
 self.addEventListener('install', function(e) {
@@ -7,52 +10,47 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
             return cache.addAll([
-                '/',
-                'index.html',
-                'service-worker.js',
-                'manifest.json',                
-                'css/app.css',
-                'css/app.ios.css',
-                'css/app.material.css',
-                'js/app.js',
-                'js/todos.js',
-                'js/init-styles.js',
-                /*'cordova.js',
-                'cordova_plugins.js',
-                'plugins/cordova-plugin-statusbar/www/statusbar.js',
-                'plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js',
-                'plugins/es6-promise-plugin/www/promise.js',
-                'plugins/phonegap-plugin-service-worker/www/service_worker_container.js',
-                'plugins/phonegap-plugin-service-worker/www/service_worker_registration.js',
-                'plugins/phonegap-plugin-service-worker/www/service_worker.js',
-                'plugins/phonegap-plugin-service-worker/www/kamino.js',*/
-                'lib/framework7/css/framework7.ios.colors.min.css',
-                'lib/framework7/css/framework7.ios.min.css',
-                'lib/framework7/css/framework7.ios.rtl.min.css',
-                'lib/framework7/css/framework7.material.colors.min.css',
-                'lib/framework7/css/framework7.material.min.css',
-                'lib/framework7/css/framework7.material.rtl.min.css',
-                'lib/framework7/js/framework7.min.js',
-                'lib/framework7-icons/framework7-icons.css',
-                'lib/framework7-icons/Framework7Icons-Regular.eot',
-                'lib/framework7-icons/Framework7Icons-Regular.ttf',
-                'lib/framework7-icons/Framework7Icons-Regular.woff',
-                'lib/framework7-icons/Framework7Icons-Regular.woff2',
-                'lib/framework7-vue/framework7-vue.min.js',
-                'lib/vue/vue.min.js', 
-                'img/Default-Portrait.png',
-                'img/Default-Portrait-736@3x.png',
-                'img/icons/apple-touch-icon.png',
-                'img/icons/mstile-150x150.png',
-                'img/icons/safari-pinned-tab.svg',
-                'img/icons/favicon-16x16.png',
-                'img/icons/favicon-32x32.png',
-                'img/icons/favicon.ico',
-                'img/icons/icon-128x128.png',
-                'img/icons/icon-144x144.png',
-                'img/icons/icon-152x152.png',
-                'img/icons/icon-192x192.png', 
-                'img/icons/icon-256x256.png',                       
+                // If we don't add the root, we must use http://localhost:3000/index.html when offline
+                '/',  
+                './index.html',
+                './manifest.json',                
+                './css/app.css',
+                './css/app.ios.css',
+                './css/app.material.css',
+                './js/app.js',
+                './js/todos.js',
+                './js/init-styles.js',
+                './cordova.js',
+                './cordova_plugins.js',
+                './plugins/cordova-plugin-statusbar/www/statusbar.js',
+                // './plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js',
+                './lib/framework7/css/framework7.ios.colors.min.css',
+                './lib/framework7/css/framework7.ios.min.css',
+                './lib/framework7/css/framework7.ios.rtl.min.css',
+                './lib/framework7/css/framework7.material.colors.min.css',
+                './lib/framework7/css/framework7.material.min.css',
+                './lib/framework7/css/framework7.material.rtl.min.css',
+                './lib/framework7/js/framework7.min.js',
+                './css/framework7-icons.css',
+                './css/Framework7Icons-Regular.eot',
+                './css/Framework7Icons-Regular.ttf',
+                './css/Framework7Icons-Regular.woff',
+                './css/Framework7Icons-Regular.woff2',
+                './lib/framework7-vue/framework7-vue.min.js',
+                './lib/vue/vue.min.js', 
+                './img/Default-Portrait.png',    
+                './img/logo.png',
+                './img/icons/apple-touch-icon.png',
+                './img/icons/mstile-150x150.png',
+                './img/icons/safari-pinned-tab.svg',
+                './img/icons/favicon-16x16.png',
+                './img/icons/favicon-32x32.png',
+                './img/icons/favicon.ico',
+                './img/icons/icon-128x128.png',
+                './img/icons/icon-144x144.png',
+                './img/icons/icon-152x152.png',
+                './img/icons/icon-192x192.png', 
+                './img/icons/icon-256x256.png',                       
             ]).then(function() {
                 self.skipWaiting();
             });
@@ -61,34 +59,45 @@ self.addEventListener('install', function(e) {
 });
 
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function(event) {
   console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheKey) {
+                    console.log("Cache key " + cacheKey);
+                    if (goodCaches.indexOf(cacheKey) === -1) {
+                        console.log("Deleting cache " + cacheKey);
+                        return caches.delete(cacheKey);    
+                    }
+                })
+            );
+        })
+    );
 });
 
-// Called when the browser fetches a url
-self.addEventListener('fetch', function(event) {  
-    // Network interception example      
-    // if (/\.png$/.test(event.request.url)) {
-    //     event.respondWith(
-    //         fetch('http://addolo.com/wp-content/uploads/2017/01/90-stunning-silly-cat-pictures-image-inspirations-1280x960-wallpapersother-wallpapers-with-captions.jpg', 
-    //         {mode: 'no-cors'}).then(function(response) {
-    //             console.log("Response from network intercept " + response);
-    //             return response;
-    //     })
-    // )}
-    //else {
-        // either respond with the cached object or go ahead and fetch the actual url    
-        event.respondWith(        
-            caches.match(event.request).then(function(response) {            
-                if (response) {
-                    // retrieved from cache
-                    console.log("[Service Worker] Retrieved object FROM CACHE for request URL -> " + event.request.url);
-                    return response;
-                }
-                // fetch as normal
-                console.log("[Service Worker] Request URL NOT found in cache, fetching... -> " + event.request.url);
-                return fetch(event.request);
-            })               
-        );
-    //}    
+self.addEventListener('fetch', function(event) {    
+  console.log('Handling fetch event for ' + event.request.url);
+  var shouldRespond = true;
+  if (/\.png$/.test(event.request.url)) {
+      shouldRespond = false;
+        event.respondWith(
+            fetch('http://addolo.com/wp-content/uploads/2017/01/90-stunning-silly-cat-pictures-image-inspirations-1280x960-wallpapersother-wallpapers-with-captions.jpg', 
+            {mode: 'no-cors'}).then(function(response) {
+                console.log("Response from network intercept " + response);
+                return response;
+        })
+  )}
+  if (shouldRespond) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log('Found response in cache:', response);
+                return response;
+            }
+            console.log('No response found in cache. Fetch from network...');
+            return fetch(event.request);
+        })               
+    );
+  }
 });
